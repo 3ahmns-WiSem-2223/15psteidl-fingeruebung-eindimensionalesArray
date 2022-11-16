a61b5417318bef3163d0dfcb04eca79d8d175c3e
Paul Steidl <62242723+PaulSteidl@users.noreply.github.com>
2022-11-16 09:01:19 +0100

    ---------------------------------

--- Assets/MyGame/Scripts/Arrays.cs
+++ Assets/MyGame/Scripts/Arrays.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class Arrays : MonoBehaviour
 {
-
     int[] array;
+    [SerializeField]
+    int[] array1 = {1, 2, 3, 4};
+    [SerializeField]
+    int[] array2 = {21, 22, 23, 24};
+    [SerializeField]
+    int[] array3 = {31, 32, 33, 34, 35, 36, 37};
+    [SerializeField]
+    string[] array4 = new string[10];
+    [SerializeField]
+    GameObject[] arrayColoredImagesRed;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < array4.Length; i++)
+        {
+            array4[i] = "array4";
+        }
+        arrayColoredImagesRed = GameObject.FindGameObjectsWithTag("RedImage");
     }
 
     // Update is called once per frame
